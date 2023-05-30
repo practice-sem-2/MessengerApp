4c0e582524ebb1b2897f8c948df193c3fbc336f7
GalteXx <65533265+GalteXx@users.noreply.github.com>
2023-05-30 22:47:50 +0300

    some urgent fixes i didnt push

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,7 +3,7 @@
 public partial class App : Application
 {
     //77.37.200.25
-    public static readonly string IP = "192.168.147.95";
+    public static readonly string IP = "77.37.200.25";
     public App()
 	{
 		InitializeComponent();
